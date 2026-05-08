d775922dee907c4ab7bef8f9a87c49e3f87be0f6
Abdullah <abdullah240804@gmail.com>
2026-05-08 16:34:46 +0530

    A mini Calculator project

---
+++ main.py
@@ -0,0 +1,34 @@
+print("*********** Welcome Calculator by Python ***********")
+num =float(input("Enter Number: "))
+Operator = ("+" , "-" , "*" , "/" , "%" , "r" , "re" , "e")
+result=num
+exit = False
+while True:
+    op =input("Enter Operator ( + - * / %, r to reset, re to result, e to exit): ")
+    if op in Operator :
+        if op == "e":
+            print("Exiting the calculator. Goodbye!")
+            exit = True
+            break
+        elif op == "+":
+            no= float(input("Enter Number: "))
+            result +=no
+        elif op == "-":
+            no= float(input("Enter Number: "))
+            result -=no
+        elif op == "*":
+            no= float(input("Enter Number: "))
+            result *=no
+        elif op == "/":
+            no= float(input("Enter Number: "))
+            result /=no
+        elif op == "%":
+            no= float(input("Enter Number: "))
+            result %=no
+        elif op == "r":
+            result = 0
+            print("Calculator reset.")
+        elif op == "re":
+            print(f"Result: {result}.")
+    else:
+        print("Invalid Input! .Try again")
